912a406cd38273e5bd43766a2aea081306ddd997
Derek Kershner <kershner@eformedpartners.com>
2019-04-08 12:24:56 -0700

    Complete with CRA

--- devenvironment/src/App.js
+++ devenvironment/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import './bootstrap.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Blog from './pages/blog/Blog';
 import Post from './pages/blog/Post';
+import Error from './Error';
 
 class App extends Component {
   render() {
-    return (      
-      <Router>        
+    return (
+      <Router>
         <Navigation />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/blog" component={Blog} />
-        <Route path="/blog/posts/:handle" component={Post} />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/portfolio' component={Portfolio} />
+          <Route exact path='/blog' component={Blog} />
+          <Route path='/blog/:handle' component={Post} />
+          <Route component={Error} status={404} />
+        </Switch>
+        <Footer />
       </Router>
     );
   }
--- devenvironment/src/pages/blog/Blog.js
+++ devenvironment/src/pages/blog/Blog.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Jumbotron } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import posts from './Posts';
 
 const Blog = () => {
-  return null;
-}
+  return (
+    <Container>
+      <Row>
+        <div className='page-header' style={{ padding: 40 }}>
+          <h1>Blog Posts</h1>
+        </div>
+      </Row>
+      {posts.map((post, index) => (
+        <PostContainer key={index} post={post} />
+      ))}
+    </Container>
+  );
+};
+
+const PostContainer = props => {
+  return (
+    <LinkContainer to={`/blog/${props.post.handle}`}>
+      <Jumbotron>
+        <h2>
+          <Link to={`/blog/${props.post.handle}`}>{props.post.title}</Link>
+        </h2>
+        <h5>{props.post.subtitle}</h5>
+        <h6>{props.post.date}</h6>
+      </Jumbotron>
+    </LinkContainer>
+  );
+};
 
 export default Blog;
