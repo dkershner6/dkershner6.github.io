c1b2b9364cfcb16f995b68cb4a56e2da1f3cc04b
Derek Kershner <kershner@eformedpartners.com>
2020-01-19 14:50:17 -0800

    Added some technologies

--- src/data/technologies-node.js
+++ src/data/technologies-node.js
@@ -1,158 +1,182 @@
 export const backendTechnologies = [
   {
-    id: 'aspNetCore',
-    label: 'ASP.NET Core / C#',
-    type: 'backend',
-    language: 'C#',
+    id: "aspNetCore",
+    label: "ASP.NET Core / C#",
+    type: "backend",
+    language: "C#",
     familiarity: 5,
-    link: 'https://dotnet.microsoft.com/apps/aspnet',
+    link: "https://dotnet.microsoft.com/apps/aspnet"
   },
   {
-    id: 'efcore',
-    label: 'Entity Framework Core / C#',
-    type: 'backend',
-    language: 'C#',
+    id: "efcore",
+    label: "Entity Framework Core / C#",
+    type: "backend",
+    language: "C#",
+    familiarity: 5
+  },
+  {
+    id: "azureFunctions",
+    label: "Azure Functions / C#",
+    type: "backend",
+    language: "C#",
     familiarity: 5,
+    link: "https://azure.microsoft.com/en-us/services/functions/"
   },
   {
-    id: 'azureFunctions',
-    label: 'Azure Functions / C#',
-    type: 'backend',
-    language: 'C#',
+    id: "azureLogicApps",
+    label: "Azure Logic Apps",
+    type: "backend",
     familiarity: 5,
-    link: 'https://azure.microsoft.com/en-us/services/functions/',
+    link: "https://azure.microsoft.com/en-us/services/logic-apps/"
   },
   {
-    id: 'azureLogicApps',
-    label: 'Azure Logic Apps',
-    type: 'backend',
-    familiarity: 5,
-    link: 'https://azure.microsoft.com/en-us/services/logic-apps/',
+    id: "node",
+    label: "Node / TypeScript",
+    type: "backend",
+    language: "Typescript",
+    familiarity: 5
   },
   {
-    id: 'node',
-    label: 'Node / JS',
-    type: 'backend',
-    language: 'Typescript',
-    familiarity: 3,
-  },
-  {
-    id: 'flask',
-    label: 'Flask / Python',
-    type: 'backend',
-    language: 'Python',
-    familiarity: 3,
-  },
+    id: "flask",
+    label: "Flask / Python",
+    type: "backend",
+    language: "Python",
+    familiarity: 3
+  }
 ];
 
 export const frontendTechnologies = [
   {
-    id: 'react',
-    label: 'React / Typescript JS',
-    type: 'frontend',
-    language: 'Typescript JS',
+    id: "react",
+    label: "React / Typescript",
+    type: "frontend",
+    language: "Typescript",
     familiarity: 5,
-    link: 'https://reactjs.org/',
+    link: "https://reactjs.org/"
   },
   {
-    id: 'liquid',
-    label: 'Liquid (Shopify)',
-    type: 'frontend',
-    language: 'Liquid',
+    id: "liquid",
+    label: "Liquid (Shopify)",
+    type: "frontend",
+    language: "Liquid",
     familiarity: 4,
-    link: 'https://help.shopify.com/en/themes/liquid',
-  },
+    link: "https://help.shopify.com/en/themes/liquid"
+  }
 ];
 
 export const deploymentTechnologies = [
   {
-    id: 'kubernetes',
-    label: 'Kubernetes / Docker',
-    type: 'deployment',
+    id: "kubernetes",
+    label: "Kubernetes / Docker",
+    type: "deployment",
     familiarity: 4,
-    link: 'https://kubernetes.io',
+    link: "https://kubernetes.io"
   },
   {
-    id: 'azureDevops',
-    label: 'Azure DevOps',
-    type: 'deployment',
+    id: "azureDevops",
+    label: "Azure DevOps",
+    type: "deployment",
     familiarity: 4,
-    link: 'https://dev.azure.com',
-  },
+    link: "https://dev.azure.com"
+  }
 ];
 
 export const dataTechnologies = [
   {
-    id: 'azureSql',
-    label: 'Azure SQL',
-    type: 'data',
-    language: 'SQL',
+    id: "dynamoDb",
+    label: "AWS DynamoDB",
+    type: "data",
+    language: "NoSQL",
     familiarity: 5,
-    link: 'https://azure.microsoft.com/en-us/services/sql-database/',
+    link: "https://aws.amazon.com/dynamodb/"
   },
   {
-    id: 'azureStorage',
-    label: 'Azure Storage Blobs / Tables',
-    type: 'data',
-    language: 'NoSQL',
+    id: "azureSql",
+    label: "Azure SQL",
+    type: "data",
+    language: "SQL",
     familiarity: 5,
-    link: 'https://azure.microsoft.com/en-us/services/storage/',
+    link: "https://azure.microsoft.com/en-us/services/sql-database/"
   },
   {
-    id: 'azureStorageQueues',
-    label: 'Azure Storage Queues',
-    type: 'data',
-    language: 'NoSQL',
+    id: "azureStorage",
+    label: "Azure Storage Blobs / Tables",
+    type: "data",
+    language: "NoSQL",
     familiarity: 5,
-    link: 'https://azure.microsoft.com/en-us/services/storage/',
+    link: "https://azure.microsoft.com/en-us/services/storage/"
   },
   {
-    id: 'redis',
-    label: 'Redis',
-    type: 'data',
-    language: 'NoSQL',
+    id: "azureStorageQueues",
+    label: "Azure Storage Queues",
+    type: "data",
+    language: "NoSQL",
+    familiarity: 5,
+    link: "https://azure.microsoft.com/en-us/services/storage/"
+  },
+  {
+    id: "redis",
+    label: "Redis",
+    type: "data",
+    language: "NoSQL",
     familiarity: 4,
-    link: 'https://redis.io/',
+    link: "https://redis.io/"
   },
   {
-    id: 'azureSearch',
-    label: 'Azure Search',
-    type: 'data',
-    language: 'NoSQL',
+    id: "azureSearch",
+    label: "Azure Search",
+    type: "data",
+    language: "NoSQL",
     familiarity: 4,
-    link: 'https://azure.microsoft.com/en-us/services/search/',
+    link: "https://azure.microsoft.com/en-us/services/search/"
   },
+  {
+    id: "mongoDb",
+    label: "Mongo DB",
+    type: "data",
+    language: "NoSQL",
+    familiarity: 4,
+    link: "https://www.mongodb.com/"
+  },
+  {
+    id: "kinesis",
+    label: "AWS Kinesis",
+    type: "data",
+    language: "Stream",
+    familiarity: 4,
+    link: "https://aws.amazon.com/kinesis/"
+  }
 ];
 
 export const clouds = [
   {
-    id: 'azure',
-    label: 'Azure',
-    type: 'cloud',
+    id: "azure",
+    label: "Azure",
+    type: "cloud",
     familiarity: 5,
-    link: 'https://azure.microsoft.com',
+    link: "https://azure.microsoft.com"
   },
   {
-    id: 'googlefirebase',
-    label: 'Google Firebase',
-    type: 'cloud',
+    id: "googlefirebase",
+    label: "Google Firebase",
+    type: "cloud",
     familiarity: 4,
-    link: 'https://firebase.google.com',
+    link: "https://firebase.google.com"
   },
   {
-    id: 'googlecloud',
-    label: 'Google Cloud',
-    type: 'cloud',
+    id: "googlecloud",
+    label: "Google Cloud",
+    type: "cloud",
     familiarity: 3,
-    link: 'https://cloud.google.com',
+    link: "https://cloud.google.com"
   },
   {
-    id: 'aws',
-    label: 'AWS',
-    type: 'cloud',
-    familiarity: 2,
-    link: 'https://aws.amazon.com/',
-  },
+    id: "aws",
+    label: "AWS",
+    type: "cloud",
+    familiarity: 5,
+    link: "https://aws.amazon.com/"
+  }
 ];
 
 function sortFamiliarityDescending(a, b) {
@@ -160,9 +184,8 @@ function sortFamiliarityDescending(a, b) {
     return 1;
   } else if (a.familiarity > b.familiarity) {
     return -1;
-  } else {
-    return 0;
   }
+  return 0;
 }
 
 export const technologies = [
@@ -170,7 +193,7 @@ export const technologies = [
   ...frontendTechnologies,
   ...deploymentTechnologies,
   ...dataTechnologies,
-  ...clouds,
+  ...clouds
 ].sort(sortFamiliarityDescending);
 
 export default technologies;
