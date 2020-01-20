465a406c7d16d2ce842d8b4ef66f6a5407514e93
Derek Kershner <kershner@eformedpartners.com>
2020-01-19 16:07:08 -0800

    Max heigh on blog thumbnails

--- src/components/BlogRollPost.tsx
+++ src/components/BlogRollPost.tsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import { Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Link } from "gatsby";
+import { Row, Col } from "react-bootstrap";
 
-import PreviewCompatibleImage from './PreviewCompatibleImage';
-import BlogPostTag from './BlogPostTag';
+import PreviewCompatibleImage from "./PreviewCompatibleImage";
+import BlogPostTag from "./BlogPostTag";
 
 const BlogRollPost = props => {
   const { post } = props;
   return (
-    <Col key={post.id} xs='12' lg='6' className='mt-3'>
+    <Col key={post.id} xs="12" lg="6" className="mt-3">
       <Row>
-        <Col xs='6'>
+        <Col xs="6">
           <Link to={post.fields.slug}>
             <PreviewCompatibleImage
               imageInfo={{
                 image: post.frontmatter.featuredimage,
-                alt: `featured image thumbnail for post ${post.title}`,
+                alt: `featured image thumbnail for post ${post.title}`
               }}
             />
           </Link>
         </Col>
-        <Col xs='6'>
+        <Col xs="6">
           <Link to={post.fields.slug}>
             <h5>{post.frontmatter.title}</h5>
           </Link>
           <p>{post.frontmatter.date}</p>
-          <p className='small'>{post.frontmatter.description}</p>
+          <p className="small">{post.frontmatter.description}</p>
           <p>
             {post.frontmatter.tags && post.frontmatter.tags.length
               ? post.frontmatter.tags.map((tag, index) => (
                   <BlogPostTag key={index} tag={tag} />
                 ))
-              : ''}
+              : ""}
           </p>
         </Col>
       </Row>
--- src/components/PreviewCompatibleImage.tsx
+++ src/components/PreviewCompatibleImage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Img from 'gatsby-image';
+import React from "react";
+import Img from "gatsby-image";
 
 interface ImageInfo {
   alt?: string;
@@ -13,8 +13,8 @@ interface PreviewCompatibleImageProps {
 }
 
 const PreviewCompatibleImage = ({ imageInfo }: PreviewCompatibleImageProps) => {
-  const imageStyle = { borderRadius: '5px' };
-  const { alt = '', childImageSharp, image } = imageInfo;
+  const imageStyle = { borderRadius: "5px", maxHeight: "175px" };
+  const { alt = "", childImageSharp, image } = imageInfo;
 
   if (!!image && !!image.childImageSharp) {
     return (
@@ -26,7 +26,7 @@ const PreviewCompatibleImage = ({ imageInfo }: PreviewCompatibleImageProps) => {
     return <Img style={imageStyle} fluid={childImageSharp.fluid} alt={alt} />;
   }
 
-  if (!!image && typeof image === 'string')
+  if (!!image && typeof image === "string")
     return <img style={imageStyle} src={image} alt={alt} />;
 
   return null;
