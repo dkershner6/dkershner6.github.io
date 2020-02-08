52d76aca0b2d99eafb4deb7366ae4a3b8a1e4e7c
Derek Kershner <kershner@eformedpartners.com>
2020-02-08 10:42:03 -0800

    Some more tests

---
+++ src/components/portfolio/__tests__/CodeChallenges.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen, fireEvent, waitForDomChange } from '@testing-library/react';
+
+import CodeChallenges from '../CodeChallenges';
+
+it('Should render', async () => {
+    const { container } = render(<CodeChallenges />);
+
+    expect(container.querySelector('h3')).toBeTruthy();
+});
