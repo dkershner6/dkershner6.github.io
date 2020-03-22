85558d37920041c80f8002b11934411b65dfa98b
Derek Kershner <kershner@eformedpartners.com>
2020-03-21 17:58:40 -0700

    Delete everything

--- src/utils/__tests__/ProperCase.test.ts
+++
@@ -1,9 +0,0 @@
-import properCase from '../ProperCase';
-
-describe('properCase', () => {
-    it('Should make proper case with no spaces or mid-word CAPS', () => {
-        const result = properCase('testTesttest');
-
-        expect(result).toEqual('Testtesttest');
-    });
-});
