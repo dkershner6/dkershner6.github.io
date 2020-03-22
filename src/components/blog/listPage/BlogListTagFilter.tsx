f76f8dee2e55d480742cffe697164e6ebd624515
Derek Kershner <kershner@eformedpartners.com>
2020-03-21 20:38:09 -0700

    Filter fix

--- src/components/blog/listPage/BlogListTagFilter.tsx
+++ src/components/blog/listPage/BlogListTagFilter.tsx
@@ -18,7 +18,7 @@ export const BlogListTagFilter = (props: IBlogListTagFilter) => {
         const allTagsUsed = [...new Set(_.flatten(posts.map((post) => post.tags)))] as string[];
         const tagsWithCount = allTagsUsed.map((tag) => ({
             tag,
-            count: posts.filter((post) => post === post.tags.includes(tag)).length
+            count: posts.filter((post) => post.tags.includes(tag)).length
         }));
 
         return (
