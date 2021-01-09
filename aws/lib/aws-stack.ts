7e4ace9e72df6952b3f4f1c0b867b56e92fabe2f
Derek Kershner <dkershner@cbtnuggets.com>
2021-01-09 13:57:37 -0800

    CDK setup and deployed

---
+++ aws/lib/aws-stack.ts
@@ -0,0 +1,23 @@
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import { AttributeType } from '@aws-cdk/aws-dynamodb';
+import * as cdk from '@aws-cdk/core';
+
+export class DKershnerStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        new dynamodb.Table(this, 'DKershnerTable', {
+            tableName: 'DKershnerTable',
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+            partitionKey: {
+                name: 'pk',
+                type: AttributeType.STRING
+            },
+            sortKey: {
+                name: 'sk',
+                type: AttributeType.STRING
+            },
+            timeToLiveAttribute: 'expiresAt'
+        });
+    }
+}
