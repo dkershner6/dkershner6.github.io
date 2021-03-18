81e4e760fbd527bd8df5e702effdaeaba2b4332f
Derek Kershner <dkershner@cbtnuggets.com>
2021-03-17 19:53:00 -0700

    Resume page skeleton ready

--- webapp/src/pages/r/[slug].tsx
+++ webapp/src/pages/r/[slug].tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
+import Error from 'next/error';
 
-const ResumeDisplayPage: NextPage = () => {
+import Resume from '../../lib/common/resume/Resume';
+import ResumeModel from '../../lib/server/models/ResumeModel';
+
+interface ResumeDisplayPageProps {
+    resume: Resume;
+}
+
+export const getServerSideProps: GetServerSideProps<
+    ResumeDisplayPageProps,
+    { slug: string }
+> = async (context) => {
+    const { slug } = context?.params;
+
+    const resume = await ResumeModel.get(slug);
+
+    return {
+        props: {
+            resume
+        }
+    };
+};
+
+const ResumeDisplayPage: NextPage<ResumeDisplayPageProps> = ({ resume }) => {
+    if (!resume) {
+        return <Error statusCode={404} />;
+    }
+
     return <div />;
 };
 
